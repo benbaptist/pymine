c0fa9a48b147ec7ae53e9fd23adb6572e71dcdd7
Ben Baptist <sawham6@gmail.com>
2011-07-07 14:25:29 -0400

    made a mistake and put item_ID.py in the root dir

---
+++ pymine/item_ID.py
@@ -0,0 +1,9 @@
+#python n00b saves the day: (what should the var name be? should this dictionary be in another array? should this be a class? this is only for obtaining block names and item/blocka IDs
+
+block_ids = {
+		"AIR": 0,
+		"STONE": 1,
+		"GRASS": 2,
+		"DIRT": 3,
+		"COBBLESTONE": 4
+		}
