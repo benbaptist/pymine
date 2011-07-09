93854d42e640e373b92272eeb98651ac0f890574
Ben Baptist <sawham6@gmail.com>
2011-07-08 20:59:37 -0400

    updated config.py and server/connection.py (I forget which)

--- pymine/server.py
+++ pymine/server.py
@@ -6,13 +6,16 @@ class Server:
         self.abort = False
         
         # TODO: Initialize logger
-        
-        # TODO: Open socket
+       	
+       	self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.socket.bind(('0.0.0.0',config{'port'}))
+		self.socket.listen(5)
     
     def listen(self):
         while not self.abort:
-            # TODO: Listen for connections
-            pass
+			c,d = self.socket.accept() # c is the client object. you can do things like c.send('blah') with it. d should return IP address, etc. (I think)
+			
+			
         
         # Loop ended, so abort = True
         sys.exit(0)
