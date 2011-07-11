f31841739bd1f1178cec70f9c6f52e77e1ed5f65
Ben Baptist <sawham6@gmail.com>
2011-07-11 12:50:03 -0400

    server now listens to client messages

--- pymine/connection.py
+++ pymine/connection.py
@@ -1,3 +1,4 @@
+import binascii
 class Connection:
 	def __init__(self, sock, addr, id, info, log):
 		self.sock = sock
@@ -7,4 +8,17 @@ class Connection:
 		self.log = log
 	
 	def listen(self):
-		pass
+		self.abort = 0
+		while not self.abort:
+			self.buffer,self.addr= self.sock.recvfrom(1024)
+			
+			if len(self.buffer) == 0:
+				self.abort = 1
+				self.log.info("Disconnected")
+				break
+			
+			if not self.abort:
+				self.log.info("Received a message")
+				self.packetID = self.buffer[0:1]
+				
+				self.log.info("Packet ID: %s" % binascii.hexlify(self.packetID))
