2a2a1e3413aec2cc50f610d7a481b5b0ed485707
Ahmed El-Mahdawy <aa.mahdawy.10@gmail.com>
2011-07-11 21:06:03 +0200

    cleaning up terrain_generator.py

--- pymine/terrain_generator.py
+++ pymine/terrain_generator.py
@@ -1,13 +1,14 @@
 class TerrainGenerator:
-	def generate(self,seed,x,y): 
-		# generates and returns a chunk. x and y are the chunk coords
+	def __init__(self):
+		pass
+	
+	def generate(self, seed, x, y): 
+		# Generates and returns a chunk. x and y are the chunk coords
 		self.blocks = {}
 		
-		#create bedrock level
+		# Create bedrock level
 		b = 0
 		c = 0
 		while b<17:
 			while c<17:
 				self.blocks.append({str(b)+str(c): 7})
-			
-		
